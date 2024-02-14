b5a5a05886f70be43170e04e389ae54cac38a059
codaltec02 <codaltec02@codaltec12.onmicrosoft.com>
2024-02-14 10:21:19 -0500

    Fixed bug in save data an database

--- src/app/api/script/route.ts
+++ src/app/api/script/route.ts
@@ -22,15 +22,6 @@ export async function POST(request: Request) {
 		const resultado = JSON.parse(resultadoJSON);
 		console.log('RESULTADO', resultado);
 		return NextResponse.json({success: true, resultado: resultado}, {status: 200});
-		// if (resultado.success) {
-		// 	// Manejar el resultado exitoso
-		// 	console.log('Resultado del script Python:', resultado.data);
-		// 	return NextResponse.json({success: true, resultado: resultado.data}, {status: 200});
-		// } else {
-		// 	// Manejar el resultado con error
-		// 	console.error('Error en el script Python:', resultado.error);
-		// 	return NextResponse.json({error: resultado.error}, {status: 500});
-		// }
 	} catch (error: any) {
 		const errorMessage = error.message || 'Error en el servidor';
 		console.error('Error en la función POST:', error);
